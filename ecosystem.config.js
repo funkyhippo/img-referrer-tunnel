48afc5a0ea3f970d5d1262ec2628bd0323d7555d
funkyhippo <funkyhippo@f-ck.me>
2021-05-13 16:34:25 -0700

    Move the keep-alive to another script, reduce instance count.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,20 @@ module.exports = {
       name: "img-proxy",
       script: "index.js",
       cwd: "./",
-      instances: "max",
+      instances: 4,
       exec_mode: "cluster",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "keep-alive",
+      script: "ping.js",
+      cwd: "./",
+      instances: 1,
       env: {
         NODE_ENV: "development",
       },
--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const server = require("./src/server");
-const { ping } = require("./src/utils");
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-  if (process.env.NODE_ENV === "production" && process.env.ACCESS_URL) {
-    setInterval(() => {
-      ping(process.env.ACCESS_URL);
-    }, 1000 * 30);
-  }
 });
