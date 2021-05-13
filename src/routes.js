ad00d7c662cb2a07fcec63e8716091b926a4b222
funkyhippo <funkyhippo@f-ck.me>
2021-05-13 16:12:22 -0700

    Add additional info to homepage.

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,12 @@ const HOME_TEXT = `
 Info here: http://github.com/funkyhippo/img-referrer-tunnel/
 
 This is a barebones implementation of the well-known cors-anywhere proxy,
-but with some additions that make image hotlinking more seamless.
+but with some additions that tailor it for image hotlinking.
+
+The resolution strategy for images is as follows:
+1. Host query parameter
+2. Known mapping in the repo
+3. Target origin
 `;
 
 const home = (req, res) => {
